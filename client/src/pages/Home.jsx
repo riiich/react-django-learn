79dacaf8bd861eeddcf43026c85e982b102b13de
riiich <horich21@gmail.com>
2024-11-22 13:37:05 -0800

    Working on retrieving, creating, and deleting notes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,9 +6,64 @@ const Home = () => {
 	const [content, setContent] = useState("");
 	const [title, setTitle] = useState("");
 
-    
+	useEffect(() => {
+		getNotes();
+	}, []);
 
-	return <div>Home</div>;
+	const getNotes = async () => {
+		try {
+			const res = await api.get("/api/notes");
+
+			setNotes(res.data);
+			console.log(res.data);
+		} catch (err) {
+			console.log("Error retrieving notes");
+			alert(err);
+		}
+	};
+
+	const createNote = async () => {
+		try {
+			const res = await api.post("/api/notes", { content, title });
+
+			if (res.status === 201) alert("Successfully created a note");
+			else alert("Failed to create a note");
+		} catch (err) {
+			console.log("Error creating a note");
+			alert(err);
+		}
+
+		getNotes();
+	};
+
+	const deleteNote = async (id) => {
+		try {
+			const res = await api.delete(`/api/notes/delete/${id}`);
+
+			if (res.status === 204) alert("Note has been deleted!");
+			else
+				alert(
+					"An error has occurred while attempting to delete this note!"
+				);
+
+			console.log(res.data);
+		} catch (err) {
+			console.log("Error deleting a note");
+			alert(err);
+		}
+
+		getNotes();
+	};
+
+	return (
+		<div>
+			<h1>Home</h1>
+
+			{notes.map((n) => {
+				return n;
+			})}
+		</div>
+	);
 };
 
 export default Home;
